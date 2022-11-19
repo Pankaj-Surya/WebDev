58e645299b0b11516f039d91df3373643a2e63a3
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2022-11-20 02:32:30 +0530

    added link profile and logout to nav

--- Module 8- React/Activity/Projects/reelsv2/src/components/Navbar.jsx
+++ Module 8- React/Activity/Projects/reelsv2/src/components/Navbar.jsx
@@ -17,7 +17,10 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import Insta from '../assets/insta.jpg'
 import '../Style/feed.css'
 
-const settings = ['Profile',  'Logout'];
+import { auth } from "../firebase"
+import { Link } from 'react-router-dom'
+
+const settings = ['Profile', 'Logout'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -38,6 +41,10 @@ function ResponsiveAppBar() {
     setAnchorElUser(null);
   };
 
+
+  async function logoutHandler() {
+    await auth.signOut();
+  }
   return (
     <AppBar position="static" className="navbar" >
       <Container maxWidth="xl">
@@ -58,25 +65,25 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-           </Typography>
+          </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            
+
           </Box>
-          
-        
+
+
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-           
+
           </Box>
 
           <Box sx={{ flexGrow: 0 }} className="nav-icons-container">
-           
+
             <HomeIcon fontSize='large' className='nav-icons' ></HomeIcon>
             <ExploreIcon fontSize='large' className='nav-icons'></ExploreIcon>
-           
+
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }} >
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{margin:"0.5rem"}} />
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{ margin: "0.5rem" }} />
               </IconButton>
             </Tooltip>
             <Menu
@@ -95,11 +102,17 @@ function ResponsiveAppBar() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link to="/profile" style={{ textDecoration: 'none', color:'black' }}>
+                  <Typography textAlign="center">Profile</Typography>
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={() => {
+                logoutHandler()
+              }}>
+                <Typography textAlign="center">Logout</Typography>
+              </MenuItem>
             </Menu>
           </Box>
         </Toolbar>
