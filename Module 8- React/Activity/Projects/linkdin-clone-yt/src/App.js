6269db78f78ceccf741d5936224ddf5e6c9490f5
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2022-12-14 00:01:35 +0530

    added template

---
+++ Module 8- React/Activity/Projects/linkdin-clone-yt/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build a linkdin clone</h1>
+      {/* Header */}
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+        
+    </div>
+  );
+}
+
+export default App;
