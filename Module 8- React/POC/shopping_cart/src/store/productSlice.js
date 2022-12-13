7a1a982134965e5703cc92c35222316e72b61b17
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2022-12-13 23:13:45 +0530

    added method 2 for midddeware

--- Module 8- React/POC/shopping_cart/src/store/productSlice.js
+++ Module 8- React/POC/shopping_cart/src/store/productSlice.js
@@ -1,4 +1,4 @@
-const { createSlice } = require('@reduxjs/toolkit');
+const { createSlice,createAsyncThunk} = require('@reduxjs/toolkit');
 
 export const STATUSES  = Object.freeze({
     IDLE:"idle",
@@ -22,13 +22,33 @@ const productSlice = createSlice({
            //  state = state.filter((item)=>item.id !== action.payload)
            state.status = action.payload;
         }
-    }
+    },
+    // extraReducers: (builder) => {
+    //     builder
+    //         .addCase(fetchProducts.pending, (state, action) => {
+    //             state.status = STATUSES.LOADING;
+    //         })
+    //         .addCase(fetchProducts.fulfilled, (state, action) => {
+    //             state.data = action.payload;
+    //             state.status = STATUSES.IDLE;
+    //         })
+    //         .addCase(fetchProducts.rejected, (state, action) => {
+    //             state.status = STATUSES.ERROR;
+    //         });
+    // },
 })
 
 export const {setProduct,setStatus} = productSlice.actions;
 export default productSlice.reducer;
 
 // thunk
+// export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+//     const res = await fetch('https://fakestoreapi.com/products');
+//     const data = await res.json();
+//     return data;
+// });
+
+
 export function fetchProducts(){
     return async function fetchProductThunk(dispatch,getState){
         dispatch(setStatus(STATUSES.LOADING))
